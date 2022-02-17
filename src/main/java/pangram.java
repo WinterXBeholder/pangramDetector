926a939d3745d9bfd54179c337528ed1690ef856
Brian <brian.hacker@ire.minnate.edu>
2022-02-17 10:22:36 -0600

    idea project files committed

--- src/main/java/pangram.java
+++ src/main/java/pangram.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class pangram {
+import java.util.Locale;
+
+public class pangram {
+    public static boolean run(String phrase) {
+        String[] characters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x", "y", "z"};
+
+        phrase = phrase.toLowerCase(Locale.ROOT);
+        int check = 0;
+        for(String ch : characters) {
+            if (!phrase.contains(ch)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/test/java/pangramTest.java
+++ src/test/java/pangramTest.java
@@ -1,4 +1,52 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class pangramTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void run() {
+        String input = "The quick brown fox jumps over the lazy dog";
+        boolean expected = true;
+        boolean actual = pangram.run(input);
+        assertEquals(expected, actual, "quick brown fox failed");
+
+        input = "T";
+        expected = false;
+        actual = pangram.run(input);
+        assertEquals(expected, actual, "\"T\" character failed");
+
+    }
+
+    @Test
+    void runNumbersTest() {
+        String input = "1 The quick brown fox jumps over the lazy dog";
+        assertEquals(true, pangram.run(input), "quick brown fox number failed");
+
+        input = "0 1 2 3 4 5 6 7 8 9";
+        boolean expected = false;
+        boolean actual = pangram.run(input);
+        assertEquals(expected, actual, "0-9 failed");
+
+        input = "0";
+        expected = false;
+        actual = pangram.run(input);
+        assertEquals(expected, actual, "0-9 failed");
+    }
+
+    @Test
+    void runSpecialCharTest() {
+        String input = "The quick brown fox jumps over the lazy dog!";
+        assertEquals(true, pangram.run(input), "quick brown fox ! failed");
+
+        input = "!@#$%^&*((_)+";
+        boolean expected = false;
+        boolean actual = pangram.run(input);
+        assertEquals(expected, actual, "!@#$%^&*( failed");
+
+        input = "!";
+        expected = false;
+        actual = pangram.run(input);
+        assertEquals(expected, actual, "\"!\" failed");
+    }
 }
